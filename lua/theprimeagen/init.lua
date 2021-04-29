6ae6832d7190b7e3819242e4fb1b41953578c590
spuny <jakubspunar@Jakubs-MacBook-Pro.local>
2021-04-29 11:12:45 +0200

    fix(Added missing functions)

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -1,5 +1,19 @@
 require("theprimeagen.telescope")
--- require("theprimeagen.git-worktree")
+require("theprimeagen.git-worktree")
 -- require("theprimeagen.harpoon")
 -- require("theprimeagen.lsp")
 -- require("theprimeagen.statusline")
+
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
