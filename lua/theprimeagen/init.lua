ec1e458b04eae6d18a20f04fffdeae59ba4d47cf
spuny <spuny4@gmail.com>
2021-04-30 09:27:40 +0200

    fix(For me this is fixing git worktree workflow, need to investigate more)

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -1,5 +1,5 @@
 require("theprimeagen.telescope")
-require("theprimeagen.git-worktree")
+-- require("theprimeagen.git-worktree")
 -- require("theprimeagen.harpoon")
 -- require("theprimeagen.lsp")
 require("theprimeagen.statusline")
