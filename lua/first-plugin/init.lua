8a43ad2ff02eaa33a6b88222a0193836dc3b2b35
spuny <spuny4@gmail.com>
2021-05-12 19:43:39 +0200

    edu(learning luca - theprimeagen)

--- lua/first-plugin/init.lua
+++ lua/first-plugin/init.lua
@@ -1,9 +1,29 @@
 local api = vim.api
-local function printWindowSize()
-  print(api.nvim_win_get_width(0), api.nvim_win_get_height(0))
+local function createFloatingWindow()
+  local stats = api.nvim_list_uis()[1]
+  local width = stats.width;
+  local height = stats.height;
+
+  local bufh = api.nvim_create_buf(false, true)
+  local winId = api.nvim_open_win(bufh, true, {
+   relative="editor",
+   width = width - 4,
+   height = height -4,
+   col = 2,
+   row = 2,
+  })
 
 end
 
+local function onResize()
+  local stats = api.nvim_list_uis()[1]
+  local width = stats.width;
+  local height = stats.height;
+
+  print("Window size", width, height)
+end
+
 return {
-  printWindowSize = printWindowSize
+  createFloatingWindow = createFloatingWindow,
+  onResize = onResize
 }
--- plugin/FirstPlugin.vim
+++ plugin/FirstPlugin.vim
@@ -1,9 +1,10 @@
 fun! FirstPlugin()
   " do not forget to remove this line !!!
   lua for k in pairs(package.loaded) do if k:match("^first%-plugin") then package.loaded[k] = nil end end
-  lua require("first-plugin").printWindowSize()
+  lua require("first-plugin").createFloatingWindow()
 endfun
 
 augroup FirstPlugin
   autocmd!
+  autocmd VimResized * :lua require("first-plugin").onResize()
 augroup END
