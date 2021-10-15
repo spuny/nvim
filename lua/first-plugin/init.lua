cec1b333e440751f8aeba417bdd5d9ca291e1283
spuny <spuny4@gmail.com>
2021-10-15 19:21:46 +0200

    fix(indent)

--- lua/first-plugin/init.lua
+++ lua/first-plugin/init.lua
@@ -6,13 +6,12 @@ local function createFloatingWindow()
 
   local bufh = api.nvim_create_buf(false, true)
   local winId = api.nvim_open_win(bufh, true, {
-   relative="editor",
-   width = width - 4,
-   height = height -4,
-   col = 2,
-   row = 2,
+    relative="editor",
+    width = width - 4,
+    height = height - 4,
+    col = 2,
+    row = 2,
   })
-
 end
 
 local function onResize()
