4a64dbf4c1f943c94e2df46341ffea59f7e2ebb7
spuny <spuny4@gmail.com>
2021-10-17 10:45:44 +0200

    feat/testing(changing themes/layouts of telescope)

--- lua/theprimeagen/telescope.lua
+++ lua/theprimeagen/telescope.lua
@@ -1,27 +1,31 @@
 local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
-        file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
-        color_devicons = true,
+      layout_strategy = "horizontal",
+      layout_config = {
+        mirror = true,
+      },
+      file_sorter = require('telescope.sorters').get_fzy_sorter,
+      prompt_prefix = ' >',
+      color_devicons = true,
 
-        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+      file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+      grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+      qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
 
-        mappings = {
-            i = {
-                ["<C-x>"] = false,
-                ["<C-q>"] = actions.send_to_qflist,
-            },
-        }
-    },
-    extensions = {
-        fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        }
-    }
+      mappings = {
+          i = {
+              ["<C-x>"] = false,
+              ["<C-q>"] = actions.send_to_qflist,
+          },
+      }
+  },
+  extensions = {
+      fzy_native = {
+          override_generic_sorter = false,
+          override_file_sorter = true,
+      }
+  }
 }
 
 require("telescope").load_extension("git_worktree")
@@ -30,6 +34,10 @@ require('telescope').load_extension('fzy_native')
 local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
+        theme = "dropdown",
+        path_display = {
+          "tail"
+        },
         prompt_title = "< VimRC >",
         cwd = "$HOME/.config/nvim",
     })
