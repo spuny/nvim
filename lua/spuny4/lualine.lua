8c47f263b25d69ac28afd59154a02ee655c6be32
spuny <spuny4@gmail.com>
2021-10-20 18:57:59 +0200

    feat(colors and branch diff)

--- lua/spuny4/lualine.lua
+++ lua/spuny4/lualine.lua
@@ -1,3 +1,16 @@
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
 require'lualine'.setup {
   options = {
     icons_enabled = true,
@@ -5,13 +18,35 @@ require'lualine'.setup {
     component_separators = {'', ''},
     section_separators = {'', ''},
     disabled_filetypes = { 'NvimTree', 'term', 'terminal', 'TelescopePrompt' },
+    lower = true,
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = {{'mode', lower = false}},
     lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_c = {
+      {'filename', file_status = true, path = 1},
+      {
+        'diff',
+        colored = true,
+        color_added = colors.green,
+        color_modified = colors.orange,
+        color_removed = colors.red,
+        symbols = { added = ' ', modified = '柳 ', removed = ' '}
+      }
+
+    },
+    lualine_x = {'encoding', 'fileformat', {'filetype', colored = true}},
+    lualine_y = {
+      'diagnostics',
+      sources = nvim_lsp,
+      sections = {'error', 'warn', 'info', 'hint'},
+      -- all colors are in format #rrggbb
+      color_error = colors.red,
+      color_warn = colors.orange,
+      color_info = colors.blue,
+      color_hint = colors.magenta,
+      symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
+    },
     lualine_z = {'location'}
   },
   inactive_sections = {
@@ -23,5 +58,5 @@ require'lualine'.setup {
     lualine_z = {}
   },
   tabline = {},
-  extensions = {'fzf', 'nvim-tree'}
+  extensions = {'fzf', 'fugitive', 'quickfix'}
 }
